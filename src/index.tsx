983af386e18f53c6c3f9c8777d73e2ed64db975e
JeffOrdze <jeff.ordze@live.ca>
2023-07-21 16:49:12 -0600

    add type check for index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,8 @@ import { GoogleOAuthProvider } from "@react-oauth/google";
 import App from "./App";
 
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root")!);
+
 root.render(
   <React.StrictMode>
     <GoogleOAuthProvider clientId="196681096733-5uk38h73nt54hasnelhg4ad0pgvcqh6i.apps.googleusercontent.com">
